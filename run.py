9e90d18284515851eda9b0a3068c56ff04745400
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-04-11 11:34:58 +0000

    Create basic game structure

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import libraries
+
+
+# Define the Battleship class
+
+    # Method to initialize attributes
+
+    # Grid size input selected by user
+
+    # List of user and computer ships
+
+    # Set to store user and computer ship coordinates
+
+    # Set to store user and computer guesses
+
+    # String to store user name
+
+    # List to store size of ships determined by grid size
+
+# Method to display the grid
+
+# Method to place ships on the grid randomly
+
+# Method to get user name and print welcome message
+
+# Method to get user guesses
+
+# method to get computer guesses
+
+# Main game loop
+
+
+# Run the game
