7e78d3b091240a645eb1973f075b06e59d6fff72
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-04-12 08:05:32 +0000

    Refactor input prompt in Battleship class to use ANSI escape codes for color in run.py

--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 # import libraries
 
-
-
+# ANSI Escape Codes
+blue = "\033[94m"
+green = "\033[92m"
+red = "\033[91m"
+end_color = "\033[0m"
 # Define the Battleship class
 class Battleship:
     # The main gameplay loop
     def play(self):
-        self.player_name = input("Please enter your name: ")
+        self.player_name = input(green + "Please enter your name: ")
         print() # Print a blank line
         welcome_message = f"Welcome to Battleship Captain {self.player_name}!\n----------------------"
         print(welcome_message) # Test the welcome message
+        print() # Print a blank line
+        rules = "Rules of Engagement:\n You will be playing against the computer.\n Each of you will have a grid with ships.\n The goal is to sink all of the opponent's ships by\n guessing their positions on the grid.\n If you hit a ship, it will be marked with 'X'.\n If you miss, it will be marked with 'O'.\n The battle continues until all ships of one player are sunk.\n-------------------------------\n"
+        print(rules) # Test the rules
+
         
 
 
