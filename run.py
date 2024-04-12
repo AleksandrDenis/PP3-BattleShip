d27d8b24951ca124607aa3e0cf83d3cb834c743d
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-04-12 08:15:00 +0000

    Refactor welcome message in Battleship class to use typewriter effect in run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # import libraries
-
+import time 
 # ANSI Escape Codes
 blue = "\033[94m"
 green = "\033[92m"
@@ -14,8 +14,11 @@ class Battleship:
         welcome_message = f"Welcome to Battleship Captain {self.player_name}!\n----------------------"
         print(welcome_message) # Test the welcome message
         print() # Print a blank line
-        rules = "Rules of Engagement:\n You will be playing against the computer.\n Each of you will have a grid with ships.\n The goal is to sink all of the opponent's ships by\n guessing their positions on the grid.\n If you hit a ship, it will be marked with 'X'.\n If you miss, it will be marked with 'O'.\n The battle continues until all ships of one player are sunk.\n-------------------------------\n"
-        print(rules) # Test the rules
+        rules = " Rules of Engagement:\n You will be playing against the computer.\n Each of you will have a grid with ships.\n The goal is to sink all of the opponent's ships by\n guessing their positions on the grid.\n If you hit a ship, it will be marked with 'X'.\n If you miss, it will be marked with 'O'.\n The battle continues until all ships of one player are sunk.\n-------------------------------\n"
+        for char in rules:
+            print(char, end='', flush=True)
+            time.sleep(0.05)
+       
 
         
 
