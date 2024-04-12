1390ee0360cc8717f93801659d719c1085d7c334
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-04-12 08:33:26 +0000

    A add input prompt for mission acceptance in run.py

--- run.py
+++ run.py
@@ -12,15 +12,22 @@ class Battleship:
         self.player_name = input(green + "Please enter your name: ")
         print() # Print a blank line
         welcome_message = f"Welcome to Battleship Captain {self.player_name}!\n----------------------"
-        print(welcome_message) # Test the welcome message
-        print() # Print a blank line
+        print(welcome_message) # Test the welcome message        
         rules = " Rules of Engagement:\n You will be playing against the computer.\n Each of you will have a grid with ships.\n The goal is to sink all of the opponent's ships by\n guessing their positions on the grid.\n If you hit a ship, it will be marked with 'X'.\n If you miss, it will be marked with 'O'.\n The battle continues until all ships of one player are sunk.\n-------------------------------\n"
         for char in rules:
             print(char, end='', flush=True)
             time.sleep(0.05)
-       
+        promt = "Do you accept the mission? Press 'Y' to accept or 'N' to decline: "
+        for char in promt:
+            print(green + char, end='', flush=True)
+            time.sleep(0.05)
+        accept_mission = input()
+        if accept_mission.lower() != 'y':
+            print(red + "Mission declined. Exiting the game." + end_color)
+            return
 
-        
+
+
 
 
 
