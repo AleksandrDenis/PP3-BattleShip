c47123324cc7786e965b43403a74e7910225c162
Denis Kudriavcev <deniskudriavcev@gmail.com>
2024-04-18 21:13:49 +0000

    Fix ship hit messages in Battleship class to remove blink effect

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ blue = "\033[94m"
 green = "\033[92m"
 red = "\033[91m"
 yellow = "\033[93m"
-blink = "\033[5m"
 end_color = "\033[0m"
 
 
@@ -123,7 +122,7 @@ class Battleship:
                     self.player_attempts.add((row, col))
                     if (row, col) in self.computer_ships_positions:
                         print(red + "Hit!" + end_color)
-                        self.computer_grid[row][col] = blink + "💥" + end_color # test blink in terminal
+                        self.computer_grid[row][col] = "💥"
                         self.computer_ships_positions.remove((row, col))
                     else:
                         print(yellow + "Miss!" + end_color)
@@ -152,7 +151,7 @@ class Battleship:
                 red + f"Computer hit your ship at ({chr(65 + row)}, {col})!" +
                 end_color
             )
-            self.player_grid[row][col] = blink + "💥" + end_color # test blink in terminal
+            self.player_grid[row][col] = "💥"
             self.player_ships_positions.remove((row, col))
         else:
             print(
@@ -195,7 +194,7 @@ class Battleship:
             print(char, end='', flush=True)
             time.sleep(0.05)
         promt = "Do you accept the mission?\
- Press 'Y' to accept or 'N' to decline: "
+ Press 'Y' to accept or any key to decline: "
         for char in promt:
             print(green + char + end_color, end='', flush=True)
             time.sleep(0.05)
